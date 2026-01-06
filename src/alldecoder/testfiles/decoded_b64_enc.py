4923b4978628449084297aac12de093f87f16680
kostyabely <kostya@thinkpad.localdomain>
2026-01-06 11:37:29 +0300

    Улучшение обработки выхода и валидации ввода

---
+++ src/alldecoder/testfiles/decoded_b64_enc.py
@@ -0,0 +1,5 @@
+# Deobfuscated with k88's tool 
+# @k88_w
+
+print("hello world!")
+a =100
--- src/alldecoder/ui/input.py
+++ src/alldecoder/ui/input.py
@@ -9,10 +9,13 @@ class CliInput:
     def __init__(self, cli_output: CliOutput):
         self.output = cli_output
 
-    def get_function_choice(self) -> str:
+    def get_function_choice(self) -> str | None:
         while True:
             user_input = input("Ваш выбор: ").strip()
-            valid_choices = [str(i) for i in range(1, 21)] + ["99"]
+            if user_input == "99":
+                return None
+
+            valid_choices = [str(i) for i in range(1, 21)]
 
             if user_input in valid_choices:
                 return user_input
@@ -20,18 +23,22 @@ class CliInput:
             self.output.print_error("Неверный выбор функции.")
             continue
 
-    def get_file_name(self) -> Tuple[str, str]:
-        file_name = input("Введите путь до файла: ")
-        file_name = file_name + ".py" if not file_name.endswith(".py") else file_name
+    def get_file_name(self) -> Tuple[str, str] | None:
+        while True:
+            file_name = input("Введите путь до файла: ").strip()
+            if file_name == "99":
+                return None
+            
+            file_name = file_name + ".py" if not file_name.endswith(".py") else file_name
 
-        if not os.path.exists(file_name):
-            self.output.print_error("Файл не существует")
-            raise FileNotFoundError()
+            if not os.path.exists(file_name):
+                self.output.print_error("Файл не существует. Введите 99 для выхода.")
+                continue
 
-        new_base_name = f"{DECODED_FILE_PREFIX}{os.path.basename(file_name)}"
-        new_file_name = os.path.join(
-            os.path.dirname(file_name), new_base_name
-        )
+            new_base_name = f"{DECODED_FILE_PREFIX}{os.path.basename(file_name)}"
+            new_file_name = os.path.join(
+                os.path.dirname(file_name), new_base_name
+            )
 
             
-        return file_name, new_file_name
+            return file_name, new_file_name
