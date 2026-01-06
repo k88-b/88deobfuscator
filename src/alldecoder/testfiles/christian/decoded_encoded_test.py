d8f1809271fc3a8b99bbc24e160230f252a54928
k88-b <k88@genocide.fun>
2026-01-06 08:46:22 +0000

    Delete src/alldecoder/testfiles directory

--- src/alldecoder/testfiles/christian/decoded_encoded_test.py
+++
@@ -1,9 +0,0 @@
-# Deobfuscated with k88's tool 
-# @k88_w
-
-'''Congratulations, you've managed to deobfuscate the easiest obfuscation ever invented by humanity.'''
-print("hello world")
-x = 4
-
-
-# comment
--- src/alldecoder/testfiles/decoded_zlib__enc.py
+++
@@ -1,5 +0,0 @@
-# Deobfuscated with k88's tool 
-# @k88_w
-
-print("hello world!")
-a =100
