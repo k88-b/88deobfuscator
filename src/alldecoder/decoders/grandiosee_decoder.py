cb9457e5a394bc7c0dd6f8acdbc34d91466bd320
kostyabely <kostya@thinkpad.localdomain>
2026-01-08 00:54:45 +0300

    add GrandioeseeObf deobfuscator

---
+++ src/alldecoder/decoders/grandiosee_decoder.py
@@ -0,0 +1,61 @@
+import re
+from ui.output import CliOutput 
+from decoders.abstract_decoder import BaseDecodersClass
+
+
+class GrandioseeObfDeobfuscator(BaseDecodersClass):
+    SOURCE_PATTERN = re.compile(
+        r"([a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\)\s*\))\s*"
+        r"\(\s*([a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\))\s*\)\s*;\s*"
+        r"[a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\)\s*\)\s*"
+        r"\(\s*([a-zA-Z0-9_]+\s*\(\s*[a-zA-Z0-9_]+\s*\))\s*\)\s*;\s*"
+        r"([a-zA-Z0-9_]+\s*\(\s*\))"
+    )
+
+    def _extract_components(self):
+        match = re.search(self.SOURCE_PATTERN, self.content)
+        if match:
+            self.main_obfuscated_block = match.group(0)
+            self.exec_wrapper = match.group(1)
+            self.arg_0 = match.group(2)
+            self.arg_1 = match.group(3)
+            self.main_func = match.group(4)
+            
+        else:
+            self.output.print_error("Pattern not found in content")
+            raise ValueError()
+
+    def _get_decode_logic(self) -> str:
+        try:
+            temp_content = self.content + f"print({self.arg_0});print({self.arg_1})"
+            output = self._capture_exec_output(temp_content)            
+            output = output.replace(self.exec_wrapper, "print")
+            return output
+        except Exception as e:
+            self.output.print_error(f"First stage decoding error: {e}")
+            raise RuntimeError(f"Failed to execute first stage decoding: {e}")
+
+    def _clean_content(self) -> None:
+        trash_pattern = r"(print\([^,]*),"
+        self.content = re.sub(trash_pattern, r"\1)#", self.content, count=1)
+
+    def decode(self) -> bool | None:
+        try:
+            self._match_obfuscation(self.SOURCE_PATTERN)
+            self._extract_components()
+            
+            self.content = self.content.replace(self.main_obfuscated_block, "")
+
+            decode_logic = self._get_decode_logic()
+
+            self.content += decode_logic + "\n" + self.main_func
+
+            self._clean_content()
+            
+            self.content = self._capture_exec_output(self.content)
+            self._write_result()            
+            return True
+            
+        except Exception as e:
+            self.output.print_error(e)
+            return None
